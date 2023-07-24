884fee164b14311b069a429dfcf636b82fdaee71
afedorov <afedorov@team.amocrm.com>
2023-07-24 14:36:17 +0300

    Вынос бизнес логики в ContactsService

--- src/Sync/Handlers/ContactsHandler.php
+++ src/Sync/Handlers/ContactsHandler.php
@@ -13,8 +13,8 @@ use Throwable;
 class ContactsHandler implements RequestHandlerInterface
 {
     /**
-     * @param ServerRequestInterface $request
-     * @return ResponseInterface
+     * @param ServerRequestInterface $request - получает HTTP запрос
+     * @return ResponseInterface - возвращает экземпляр класса JsonResponse c ответом
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
@@ -22,38 +22,11 @@ class ContactsHandler implements RequestHandlerInterface
             $queryParams = $request->getQueryParams();
             $apiClient = new ApiService();
             $contactsService = new ContactsService($apiClient);
-
             $accountId = $queryParams["id"] ?? $_SESSION['service_id'];
-            $token = $apiClient->readToken($accountId);
-            $data = $contactsService->getContacts($token);
-            $contacts = json_decode($data, true);
-
-            $result = [];
-
-            foreach ($contacts as $contact) {
-                $name = $contact['name'];
-                if ($contact['custom_fields_values'] === null) {
-                    $email = null;
-                } else {
-                    foreach ($contact['custom_fields_values'] as $field) {
-                        if (isset($field['field_code']) && $field['field_code'] === 'EMAIL') {
-                            foreach ($field['values'] as $value) {
-                                if (isset($value['enum_code']) && $value['enum_code'] === 'WORK') {
-                                    $email[] = $value['value'];
-                                }
-                            }
-                        }
-                    }
-                }
-
-                $result[] = [
-                    'name' => $name,
-                    'email' => $email,
-                ];
-            }
+            $data = $contactsService->getNameAndEmail($accountId);
 
             return new JsonResponse([
-                'result' => $result
+                'result' => $data
             ]);
         } catch (Throwable $e) {
             return new JsonResponse(["message" => $e->getMessage()]);
--- src/Sync/Services/ContactsService.php
+++ src/Sync/Services/ContactsService.php
@@ -2,15 +2,16 @@
 
 namespace Sync\Services;
 
-use AmoCRM\Collections\ContactsCollection;
 use AmoCRM\Exceptions\AmoCRMApiException;
 use Exception;
 use League\OAuth2\Client\Token\AccessToken;
-use Psr\Container\ContainerInterface;
+use Throwable;
+
 
 class ContactsService
 {
     private ApiService $apiService;
+
 
     /**
      * @param ApiService $apiService
@@ -23,9 +24,10 @@ class ContactsService
     /**
      * Получение контактов по токену
      *
+     * @return array - возвращаем массив контактов
      * @throws Exception
      */
-    public function getContacts(AccessToken $accessToken): ?ContactsCollection
+    public function getContacts(AccessToken $accessToken): ?array
     {
         $container = require 'config/container.php';
         $logger = $container->get('logger');
@@ -42,7 +44,7 @@ class ContactsService
                 ->setAccountBaseDomain($tokenData['base_domain'])
                 ->setAccessToken($accessToken)
                 ->contacts()
-                ->get();
+                ->get()->toArray();
         } catch (AmoCRMApiException $e) {
             $logger->logger->info('Error get contacts for file ' . __FILE__ . ', line ' . __LINE__);
 
@@ -51,4 +53,52 @@ class ContactsService
 
         return $contacts;
     }
+
+    /**
+     * Получаем имя и эмейл контактов
+     *
+     * @throws Exception
+     * @return array возвращаем массив с именами и эмейлами контактов
+     */
+    public function getNameAndEmail($accountId): ?array
+    {
+        $container = require 'config/container.php';
+        $logger = $container->get('logger');
+
+        try {
+            $token = $this->apiService->readToken($accountId);
+            $data = $this->getContacts($token);
+
+            $result = [];
+
+            foreach ($data as $contact) {
+                $email = [];
+                $name = $contact['name'];
+                if ($contact['custom_fields_values'] === null) {
+                    $email = null;
+                } else {
+                    foreach ($contact['custom_fields_values'] as $field) {
+                        if (isset($field['field_code']) && $field['field_code'] === 'EMAIL') {
+                            foreach ($field['values'] as $value) {
+                                if (isset($value['enum_code']) && $value['enum_code'] === 'WORK') {
+                                    $email[] = $value['value'];
+                                }
+                            }
+                        }
+                    }
+                }
+
+                $result[] = array(
+                    'name' => $name,
+                    'email' => $email,
+                );
+            }
+        } catch (AmoCRMApiException $e) {
+            $logger->logger->info('Error get contacts for file ' . __FILE__ . ', line ' . __LINE__);
+            throw new Exception('Error get [name,email] contacts');
+        }
+
+        return $result;
+    }
+
 }
